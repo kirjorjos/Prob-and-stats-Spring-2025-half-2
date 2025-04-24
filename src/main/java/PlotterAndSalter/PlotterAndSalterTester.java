3ee4f9913ccb5fe3608adb981875d4e14b4643d0
kirjorjos <kirjorjos@comcast.net>
2025-04-24 14:56:47 -0400

    Push what I have rn

--- src/main/java/PlotterAndSalter/PlotterAndSalterTester.java
+++ src/main/java/PlotterAndSalter/PlotterAndSalterTester.java
@@ -1,19 +1,36 @@
 package PlotterAndSalter;
 
+import java.io.File;
 import java.io.IOException;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class PlotterAndSalterTester {
 	public static void main(String[] args) throws IOException, CloneNotSupportedException {
 		Function function = Plotter.getFunction("(x^3)+(x^2)+3x+1");
+		
 		XYSeries initialCoords = Plotter.graphFunction(function, -50, 50);
-		XYSeries saltedCoords = Salter.salt(initialCoords, 10);
-		XYSeries smoothedCoords = Smoother.smooth(saltedCoords, 5, true);
+		XYSeries saltedCoords = Salter.salt(initialCoords, 10000);
+		XYSeries smoothedCoords = Smoother.smooth(saltedCoords, 10, true);
+		
 		System.out.println("Initial Coords: " + initialCoords.getItems());
 		System.out.println("Salted Coords: " + saltedCoords.getItems());
-		System.out.println("Smothed Coords: " + smoothedCoords.getItems());
+		System.out.println("Smoothed Coords: " + smoothedCoords.getItems());
+		
 		DataHandler.write(initialCoords, "Initial.csv");
 		DataHandler.write(saltedCoords, "Salted.csv");
 		DataHandler.write(smoothedCoords, "Smoothed.csv");
+		
+		JFreeChart initialPlot = ChartFactory.createScatterPlot("Initial Coordinates", "X", "Y", new XYSeriesCollection(initialCoords));
+		JFreeChart saltedPlot = ChartFactory.createScatterPlot("Salted Coordinates", "X", "Y", new XYSeriesCollection(saltedCoords));
+		JFreeChart smoothedPlot = ChartFactory.createScatterPlot("Smoothed Coordinates", "X", "Y", new XYSeriesCollection(smoothedCoords));
+		
+		ChartUtils.saveChartAsPNG(new File("initial.png"), initialPlot, (int) Math.min(1200, Math.max(400, 50*(initialCoords.getMaxX()-initialCoords.getMinX()))), (int) Math.min(1000, Math.max(400, 50*(initialCoords.getMaxY()-initialCoords.getMinY()))));
+		ChartUtils.saveChartAsPNG(new File("salted.png"), saltedPlot, (int) Math.min(1200, Math.max(400, 50*(saltedCoords.getMaxX()-saltedCoords.getMinX()))), (int) Math.min(1000, Math.max(400, 50*(saltedCoords.getMaxY()-saltedCoords.getMinY()))));
+		ChartUtils.saveChartAsPNG(new File("smoothed.png"), smoothedPlot, (int) Math.min(1200, Math.max(400, 50*(smoothedCoords.getMaxX()-smoothedCoords.getMinX()))), (int) Math.min(1000, Math.max(400, 50*(smoothedCoords.getMaxY()-smoothedCoords.getMinY()))));
 	}
 }
--- src/main/java/PlotterAndSalter/Smoother.java
+++ src/main/java/PlotterAndSalter/Smoother.java
@@ -1,36 +1,40 @@
 package PlotterAndSalter;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYSeries;
 
 public class Smoother {
 
-	public static XYSeries smooth(XYSeries saltedCoords, int windowValue, boolean useNewCoords) throws CloneNotSupportedException {
-		XYSeries newCoords = (XYSeries) saltedCoords.clone();
-		int dataSize = saltedCoords.getItemCount();
-		for (int i = 0; i < dataSize; i++) {
-			ArrayList<Double> tempYs = new ArrayList<Double>();
-			for (int j = (Math.max(0, i-windowValue)); j < Math.min(dataSize, i+windowValue); j++) {
-				if (useNewCoords) {
-					tempYs.add((double) newCoords.getDataItem(j).getY());
-				} else {
-					tempYs.add((double) saltedCoords.getDataItem(j).getY());
-				}
-			}
-			newCoords.add(new XYDataItem((double) newCoords.getDataItem(i).getX(), average(tempYs.toArray(new Double[0]))));
-		}
-		return newCoords;
-		
-	}
-	
-	private static double average(Double[] values) {
-		double total = 0;
-		for (double value : values) {
-			total += value;
-		}
-		return total/values.length;
-	}
+	public static XYSeries smooth(XYSeries saltedCoords, int windowValue, boolean useNewCoords) {
+		XYSeries smoothedCoords = new XYSeries("Smoothed");
+		List<XYDataItem> saltedCoordsList = saltedCoords.getItems();
 
+		int windowSize = windowValue * 2 + 1;
+		int saltedCoordsSize = saltedCoordsList.size();
+
+		ArrayList<Double> workingY = new ArrayList<>();
+		for (XYDataItem item : saltedCoordsList) {
+			workingY.add(item.getYValue());
+		}
+
+		for (int i = windowValue; i < (saltedCoordsSize - windowValue); i++) {
+			double sum = 0;
+			for (int j = i - windowValue; j <= i + windowValue; j++) {
+				sum += workingY.get(j);
+			}
+			double average = sum / windowSize;
+			double x = saltedCoordsList.get(i).getXValue();
+			smoothedCoords.add(x, average);
+
+			if (useNewCoords) {
+				workingY.set(i, average);
+			}
+		}
+		
+		return smoothedCoords;
+	}
 }
